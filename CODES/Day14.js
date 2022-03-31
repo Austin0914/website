c12df1660506d75cdb7d1f8206cfdc01512695d1
Austin0914 <87524840+Austin0914@users.noreply.github.com>
2022-03-31 23:16:42 +0800

    Add files via upload

---
+++ CODES/Day14.js
@@ -0,0 +1,37 @@
+let Name="Austin"
+let money=100
+let jud=true
+
+console.log(Name)
+console.log(money)
+console.log(jud)
+
+let abc = ["Austin","Billy"]
+console.log(abc)
+abc.push('Bojji')
+console.log(abc)
+console.log(abc.length)
+
+const post=[
+    {
+        image: '123',
+        date: '2022/3/30'
+    },
+    {
+        image: '222',
+        date: '2022/3/31'
+    }
+
+]
+console.log(post)
+
+let a=10
+let b=10
+if(a===b){
+    console.log("Yes")
+}else{
+    console.log("No")
+}
+for(let i=0;i<post.length;i++){
+    console.log(post[i])
+}
