df06228665e220aeb0d0337cd3827c40d1bfd852
Austin0914 <87524840+Austin0914@users.noreply.github.com>
2022-05-04 00:57:43 +0800

    Add files via upload

---
+++ CODES/app.js
@@ -0,0 +1,19 @@
+/** 
+ * === ==
+ * != !==
+ * > < >= <=
+ * || &&
+ * Number() toString NaN
+ * turthy vs falsy
+ * false,0,"",null,undefined
+ * array
+ * index length push pop shift unshift
+ * fuction parameter return
+ */
+
+function call(name){
+    alert("Welcome "+name);
+}
+
+let input=prompt("Please Enter Your Name in English!");
+call(input)
