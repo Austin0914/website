ae2cf24ae8c99deda0c43219c57c4bf5a9365917
Austin0914 <austin091493blue@gmail.com>
2022-06-02 23:47:37 +0800

    Personal Build Web css and animation

--- Personal_Website/js/app.js
+++ Personal_Website/js/app.js
@@ -1,25 +1,33 @@
-var wrapper = document.querySelector('.icon-1 svg')
-var wrapper_2 = document.querySelector('.icon-2 svg')
-var wrapper_3 = document.querySelector('.icon-3 svg')
+const wrapper = document.querySelector('.icon-1 svg')
+const wrapper_2 = document.querySelector('.icon-2 svg')
+const wrapper_3 = document.querySelector('.icon-3 svg')
 
 setTimeout(()=>{wrapper.classList.add('active');},10)
 setTimeout(()=>{wrapper_2.classList.add('active');},700)
 setTimeout(()=>{wrapper_3.classList.add('active');},1500)
 
-const last=document.querySelector(".animation");
-const last_1=document.querySelector(".icon-4");
 
-last_1.addEventListener("animationend",()=>{
+const last_1=document.querySelector(".icon-4");
+wrapper_2.addEventListener("animationend",()=>{
     const element_g = document.getElementById("green-logo");
     const element_b = document.getElementById("blue-logo");
-    const element_gr = document.getElementById("gray-logo");
-    element_gr.remove();
     element_g.remove();
     element_b.remove();
 })
 
-last.addEventListener("animationend",()=>{
-    const element = document.getElementById("st");
-    last.style="transition: all 1s ease;opacity:0;pointer-events:none;";
-    setTimeout(()=>{element.remove();},1500)
+
+last_1.addEventListener("animationend",()=>{
+    const element_gr = document.getElementById("gray-logo");
+    element_gr.remove();
 })
+
+const last=document.querySelector(".animation");
+
+last.addEventListener("animationend",()=>{
+    last.style="transition: all 1s;opacity:0;";
+    const body_srcollable=document.body;
+    body_srcollable.style.overflow="auto";
+    setTimeout(()=>{last.remove();},1400);
+})
+
+
